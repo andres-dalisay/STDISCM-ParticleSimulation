9434fac4629d4eadcb28b10a5722d1fae0e2b302
andres_dalisay <andres_dalisay@dlsu.edu.ph>
2024-02-09 17:14:58 +0800

    added FPS counter

--- STDISCM-ParticleSimulation/main.cpp
+++ STDISCM-ParticleSimulation/main.cpp
@@ -27,13 +27,71 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <math.h>
+#include<chrono>
 
 #define PI 3.14159265358979323846
 
+class FPS
+{
+public:
+	/// @brief Constructor with initialization.
+	///
+	FPS() : mFrame(0), mFps(0) {}
+
+	/// @brief Update the frame count.
+	/// 
+
+
+	/// @brief Get the current FPS count.
+	/// @return FPS count.
+	const unsigned int getFPS() const { return mFps; }
+
+private:
+	unsigned int mFrame;
+	unsigned int mFps;
+	sf::Clock mClock;
+
+public:
+	void update()
+	{
+		if (mClock.getElapsedTime().asSeconds() >= 1.f)
+		{
+			mFps = mFrame;
+			mFrame = 0;
+			mClock.restart();
+		}
+
+		++mFrame;
+	}
+};
+
 int main()
 {
+
 	// Create the window
 	sf::RenderWindow window(sf::VideoMode(1280, 720), "Particle Simulator");
+	window.setFramerateLimit(60);
+
+	std::chrono::high_resolution_clock::time_point start;
+	std::chrono::high_resolution_clock::time_point end;
+
+
+	auto lastDrawTime = std::chrono::steady_clock::now();
+	const std::chrono::milliseconds interval(500); // 0.5 seconds
+	FPS fps;
+
+	sf::Font font;
+	if (!font.loadFromFile("OpenSans-VariableFont_wdth,wght.ttf"))
+	{
+		std::cout << "error";
+	}
+
+	sf::Text fpsText;
+	fpsText.setFont(font);
+	fpsText.setCharacterSize(100);
+	fpsText.setFillColor(sf::Color::Red);
+	fpsText.setPosition(100, 100);
+	fpsText.setStyle(sf::Text::Bold | sf::Text::Underlined);
 
 	// Create the ball shape
 	sf::CircleShape ball(10);
@@ -60,14 +118,21 @@ int main()
 	// Main loop
 	while (window.isOpen())
 	{
-		// Process events
+		auto currentTime = std::chrono::steady_clock::now();
+		auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(currentTime - lastDrawTime);
+
+	
+	// Process events
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
+		// window.draw, etc.
+		
 
+		
 		// Update ball position
 		ball.move(velocityX, velocityY);
 
@@ -83,6 +148,20 @@ int main()
 		// Draw the ball
 		window.draw(ball);
 
+
+
+		fps.update();
+		/*fpsText = std::to_string(fps.getFPS());*/
+
+		
+
+		if (elapsedTime >= interval) {
+			// Update last draw time
+			lastDrawTime = currentTime;
+			fpsText.setString(std::to_string(fps.getFPS()));
+		}
+		window.draw(fpsText);
+		
 		// Display the contents of the window
 		window.display();
 	}
